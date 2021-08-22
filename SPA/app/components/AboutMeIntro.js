68e82ab26e06e19a2ab1642b34bd6e2d4f64608e
Ezequiel <ezequieldbo22@hotmail.com>
2021-08-21 21:26:53 -0300

    improved aboutMeIntro and aboutMeIntroCards components. Added a button for show more about me and an animation for cards

--- SPA/app/components/AboutMeIntro.js
+++ SPA/app/components/AboutMeIntro.js
@@ -2,25 +2,73 @@ import { AboutMeIntroCards } from "./AboutMeIntroCards.js";
 
 export function AboutMeIntro(){
 
+    // one of main conteiner of aboutMeIntro and title of section
     const $conteinerIntro = document.createElement("div"),
-    $aboutMeTitle = document.createElement("h6"),
+    $aboutMeTitle = document.createElement("h6");
+
+    // conteiner of text and button
+    const $boxAboutMeConteiner = document.createElement("div"),
+
+    // box of text and text himself
     $boxAboutMe = document.createElement("div"),
+    $textBox = document.createElement("div"),
     $text = document.createElement("p");
 
+    // button box and button himself
+    const $buttonBox = document.createElement("div"),
+    $button = document.createElement("p");
+
+    // classes
+
+    $conteinerIntro.classList.add("conteiner");
     $conteinerIntro.classList.add("aboutMeIntro");
-    $boxAboutMe.classList.add("boxAboutMe")
+
+    // class of text and button
+
+    $boxAboutMeConteiner.classList.add("boxAboutMeConteiner");
+
+    // classes of text
+
+    $boxAboutMe.classList.add("boxAboutMe");
+    $textBox.classList.add("textBox");
+    
+
+    // classes of button
+    $buttonBox.classList.add("buttonBox");
+    $button.classList.add("btnAboutMe");
+
+    // title and text content
 
     $aboutMeTitle.innerHTML = "Sobre Mi";
     $text.innerHTML = `Mi nombre es Ezequiel, hace 6 años aproximadamente que practico programación
     haciendo pequeños ejemplos de código con C, Java, MySQL y demás.
-    <br>Pero no fue hasta mediados de 2020, en medio de la pandemia, que decidi volcarme 
-    totalmente en la programación web`;
-    
+    Pero no fue hasta mediados de 2020, en medio de la pandemia, que encontre distintos proyectos que queria hacer
+    y la programacion web fue el mejor camino para llevarlos a cabo`;
 
+    // button
+    $button.innerHTML = `<a href="#">Mas Sobre mi</a>`;
+    
+    // appeding main divs
     $conteinerIntro.appendChild($aboutMeTitle);
-    $conteinerIntro.appendChild($boxAboutMe);
-    $boxAboutMe.appendChild($text);
-    $conteinerIntro.appendChild(AboutMeIntroCards());
+    $conteinerIntro.appendChild($boxAboutMeConteiner);
+
+    // added the text to the box text
+    $textBox.appendChild($text);
+
+    // added the box in the text box
+    $boxAboutMe.appendChild($textBox);
+
+    // added the button in his div
+    $buttonBox.appendChild($button);
+
+    // added button div in boxAboutMe div
+    $boxAboutMe.appendChild($buttonBox);
+
+    // added the text box and button box in his main div
+    $boxAboutMeConteiner.appendChild($boxAboutMe);
+
+    // added component of cards/box (interest, softskills and hobbies)
+    $boxAboutMeConteiner.appendChild(AboutMeIntroCards());
 
 
     return $conteinerIntro;
--- SPA/app/components/AboutMeIntroCards.js
+++ SPA/app/components/AboutMeIntroCards.js
@@ -1,53 +1,95 @@
 export function AboutMeIntroCards(){
 
     const $doc = document,
-    $conteinerCards = $doc.createElement("div"),
-    $boxInterest  = $doc.createElement("div"),
+    $conteinerCards = $doc.createElement("div");
+
+    // box, title, list and animation's div of interest 
+    const $boxInterest  = $doc.createElement("div"),
     $titleInterest = $doc.createElement("p"),
     $ulInterest = $doc.createElement("ul"),
-    $boxSoftSkills = $doc.createElement("div"),
+    $interestAnimacion = $doc.createElement("div");
+
+    // box, title, list and animation's div of soft skills
+    const $boxSoftSkills = $doc.createElement("div"),
     $titleSkills = $doc.createElement("p"),
     $ulSoftSkills = $doc.createElement("ul"),
-    $boxHobbies = $doc.createElement("div"),
-    $titleHobbies = $doc.createElement("p"),
-    $ulHobbies = $doc.createElement("ul");
+    $softSkillsAnimacion = $doc.createElement("div");
 
+    // box, title, list and animation's div of hobbies
+    const $boxHobbies = $doc.createElement("div"),
+    $titleHobbies = $doc.createElement("p"),
+    $ulHobbies = $doc.createElement("ul"),
+    $hobbiesAnimacion = $doc.createElement("div");
+
+
+    // classes
     $conteinerCards.classList.add("conteinerCards");
+
+    // interest classes
+
     $boxInterest.classList.add("boxInterest");
+    $boxInterest.classList.add("box");
+    $interestAnimacion.classList.add("background-hover");
+
+     // soft skills classes
     $boxSoftSkills.classList.add("boxSoftSkills");
+    $boxSoftSkills.classList.add("box");
+    $softSkillsAnimacion.classList.add("background-hover");
+
+    // hobbies classes
     $boxHobbies.classList.add("boxHobbies");
+    $boxHobbies.classList.add("box");
+    $hobbiesAnimacion.classList.add("background-hover");
+
+    // textual content of interest box
 
     $titleInterest.innerHTML = "Intereses";
 
     $ulInterest.innerHTML = `
-    <li>Programacion</li>
-    <li>Aprender</li>
-    <li>Memes</li>`;
+    <li>- Programacion</li>
+    <li>- Aprender</li>
+    <li>- Memes</li>
+    <li>- Mi Familia</li>
+    <li>- Libros</li>`;
+
+    // textual content of soft skills box
 
     $titleSkills.innerHTML = "Habilidades Blandas";
 
     $ulSoftSkills.innerHTML = `
-    <li>Pro Activo</li>
-    <li></li>
-    <li></li>`;
+    <li>- Autodidacta</li>
+    <li>- Pro Activo</li>
+    <li>- Persistente</li>
+    <li>- Manejo del Tiempo</li>
+    <li>- Amable</li>`;
 
+    // textual content of hobbies box
 
     $titleHobbies.innerHTML = "Hobbies";
 
     $ulHobbies.innerHTML = `
-    <li>Traducir</li>
-    <li>Juegos</li>
-    <li></li>`;
+    <li>- Traducir</li>
+    <li>- Juegos</li>
+    <li>- Diseñar obj 3d</li>`;
     
+
+    // appending of interest
 
     $boxInterest.appendChild($titleInterest);
     $boxInterest.appendChild($ulInterest);
+    $boxInterest.appendChild($interestAnimacion);
+
+    // appending of soft skills
 
     $boxSoftSkills.appendChild($titleSkills);
     $boxSoftSkills.appendChild($ulSoftSkills);
+    $boxSoftSkills.appendChild($softSkillsAnimacion);
+
+    // appending of hobbies
     
     $boxHobbies.appendChild($titleHobbies);
     $boxHobbies.appendChild($ulHobbies);
+    $boxHobbies.appendChild($hobbiesAnimacion);
 
     // add the box at conteiner
 
