85156b487905a1c5bfa85c1feed0283d16988c4a
Ezequiel <ezequieldbo22@hotmail.com>
2021-08-23 11:27:54 -0300

    maked a bit more big icons and a scale animation at footer icons. changed width of icons and name

--- SPA/app/components/Footer.js
+++ SPA/app/components/Footer.js
@@ -1,28 +1,32 @@
 export function Footer(){
 
     const $footer = document.createElement("footer"),
+    $footerConteiner = document.createElement("div"),
     $iconsBox = document.createElement("div"),
     $ulIcons = document.createElement("ul"),
     $authorBox = document.createElement("div"),
     $nameAuthor = document.createElement("p");
 
     $footer.classList.add("footer");
+    $footerConteiner.classList.add("footerConteiner");
     $iconsBox.classList.add("iconsBox");
     $ulIcons.classList.add("iconList");
     $authorBox.classList.add("authorBox");
 
     $ulIcons.innerHTML = `
     <li><a href=""><span class="icons"><i class="fab fa-github"></i></span></a></li>
-    <li><a href=""><span class="icons"><i class="fab fa-linkedin"></i></span></a></li>
-    <li><a href=""><span class="icons"><i class="fas fa-envelope"></i></span></a></li>`
+    <li><a href=""><span class="icons"><i class="fab fa-linkedin"></i></span></a></li>`
 
     $nameAuthor.innerHTML = `© Ezequiel Bonino`
 
 
     $iconsBox.appendChild($ulIcons);
     $authorBox.appendChild($nameAuthor);
-    $footer.appendChild($iconsBox);
-    $footer.appendChild($authorBox);
+
+    $footerConteiner.appendChild($iconsBox);
+    $footerConteiner.appendChild($authorBox);
+    $footer.appendChild($footerConteiner);
+    
 
 
     return $footer;
