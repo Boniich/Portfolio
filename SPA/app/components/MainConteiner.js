37b315cca86387269a5b20601d0ff64b8a8d733c
Ezequiel <ezequieldbo22@hotmail.com>
2021-08-29 18:54:53 -0300

    mainConteiner come back at old struct. I did struct in other way for dont have some struct problems

--- SPA/app/components/MainConteiner.js
+++ SPA/app/components/MainConteiner.js
@@ -1,17 +1,9 @@
 export function MainConteiner(){
 
-    const $mainConteiner = document.createElement("div"),
-    $conteiner =  document.createElement("div"),
-    $boxMain = document.createElement("div");
+    const $mainConteiner = document.createElement("div");
 
     $mainConteiner.id = "main-conteiner";
-    $conteiner.classList.add("conteiner");
-    $boxMain.id = "box-main"
-    $boxMain.classList.add("box-main");
 
-    $conteiner.appendChild($boxMain);
-    $mainConteiner.appendChild($conteiner);
-  
 
     return $mainConteiner; 
 }
