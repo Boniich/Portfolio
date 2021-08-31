135089363f5ed4a3845f408895d311d6f42a19cc
Ezequiel <ezequieldbo22@hotmail.com>
2021-08-31 13:56:24 -0300

    responsive nav doesnt work like is necesary, so normal nav is enabled temporaly

--- SPA/app/components/Nav.js
+++ SPA/app/components/Nav.js
@@ -2,13 +2,40 @@ export function Nav(){
 
     const $navConteiner = document.createElement("div"),
     $nav = document.createElement("nav"),
+    $label = document.createElement("label"),
     $ul = document.createElement("ul");
 
     $navConteiner.id = "nav-id";
     $navConteiner.classList.add("nav-conteiner"),
     $nav.classList.add("nav");
+    $label.classList.add("icon");
+    $ul.classList.add("navList");
+
+    
+    // addEventListener("DOMContentLoaded", () =>{
+    //     const button = document.querySelector(".icon");
+
+    //     button.addEventListener("click", ()=>{
+    //         if(button){
+    //             const menu = document.querySelector(".navList");
+    //             menu.classList.toggle("open");
+    //             const n = document.querySelector(".navList.open");
+               
+
+    //             if(n){
+    //                 document.body.style.overflow = "hidden";
+    //             }else{
+    //                 document.body.style.overflow = "scroll";
+    //             }
+                
+    //         }
+    //     })
+    // })
+
 
     // Creamos <li> y damos textos a los link del menu
+
+    $label.innerHTML = `<i class="fas fa-bars"></i>`;
 
     $ul.innerHTML = `
     <li><a href="#/">Inicio</a></li>
@@ -16,7 +43,7 @@ export function Nav(){
     <li><a href="#/proyectos">Proyectos</a></li>
     <li><a href="#/contactame">Contactame</a></li>
     `
-
+    $nav.appendChild($label);
     $nav.appendChild($ul);
 
     $navConteiner.appendChild($nav);
