85d134adcf627bdadbf3fe4a4ad87fbd6c74ba7b
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-10-02 10:23:15 -0300

    added personal image

--- SPA/app/components/Hero.js
+++ SPA/app/components/Hero.js
@@ -3,13 +3,27 @@ import { HeroTexts } from "./HeroTexts.js";
 export function Hero(){
 
     const $hero = document.createElement("div"),
-    $heroContent = document.createElement("div");
+    $heroContent = document.createElement("div"),
+    $conteinerPersonalImg = document.createElement("div"),
+    $boxPersonalImg = document.createElement("div"),
+    $PersonalImg = document.createElement("img");
+
+    $PersonalImg.src = 'app/assets/img/personalImg.png';
 
     $hero.classList.add("hero");
     $heroContent.classList.add("hero-content");
+    $conteinerPersonalImg.classList.add("conteiner-personal-img");
+    $boxPersonalImg.classList.add("box-personal-img");
+
+
+    $boxPersonalImg.appendChild($PersonalImg);
+
 
     $heroContent.appendChild(HeroTexts());
+    $conteinerPersonalImg.appendChild($boxPersonalImg);
+    $heroContent.appendChild($conteinerPersonalImg);
     $hero.appendChild($heroContent);
+    
 
 
 
