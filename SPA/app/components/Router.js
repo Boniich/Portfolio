6953e50066decce78ef48d1c208732cc92c02a4f
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-10-03 17:40:04 -0300

    Ajax who show proyects is now in a helper component and is called like a function. Deleted some lines of code related with body color

--- SPA/app/components/Router.js
+++ SPA/app/components/Router.js
@@ -1,16 +1,14 @@
-import { ajax } from "../helpers/ajax.js";
 import { AboutMe } from "./AboutMe.js";
 import { ContactMe } from "./ContactMe.js";
 import { Hero } from "./Hero.js";
 import { SecondaryHero } from "./SecondaryHero.js";
 import { Main } from "./Main.js";
-import { ProyectsCards } from "./ProyectsCards.js";
+import { getProyects } from "../helpers/getProyects.js";
 
 
 export async function Router(){
 
     const $doc = document,
-    $body = $doc.getElementById("body-color"),
     $nav = $doc.getElementById("nav-id"),
     $main = $doc.getElementById("main-conteiner"),
     $heroConteiner = $doc.getElementById("heroConteiner");
@@ -20,7 +18,6 @@ export async function Router(){
     if(!hash || hash === "#/"){
         $heroConteiner.appendChild(Hero());
         $main.appendChild(Main());
-
     }else if (hash === "#/sobremi"){
 
         $heroConteiner.appendChild(SecondaryHero());
@@ -29,30 +26,13 @@ export async function Router(){
     }else if (hash === "#/proyectos"){
 
         $heroConteiner.appendChild(SecondaryHero());
-        // remove the class body-color because i cannot do give white color
-        // like main and about me
-        document.body.classList.remove("body-color");
         $main.classList.add("grid-fluid");
-
-        await ajax({
-            url: `app/assets/json/proyects.json`,
-            cbSuccess: (proyects) =>{
-                console.log(proyects);
-                proyects.forEach(proyect => {
-
-                   $main.appendChild(ProyectsCards(proyect));
-                });
-
-            }
-
-        });
+        //call async function that render proyects cards
+        getProyects($main);
 
     }else{
 
-       
-        document.body.classList.add("body-color");
         $nav.style.position = "relative";
-        $heroConteiner.style.display = "none";
         $main.appendChild(ContactMe());
 
     }
