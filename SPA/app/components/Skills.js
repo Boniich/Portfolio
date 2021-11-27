01c684c511242e1938529a49a8803a24f44ec636
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-26 21:09:20 -0300

    added new skills

--- SPA/app/components/Skills.js
+++ SPA/app/components/Skills.js
@@ -12,11 +12,7 @@ export function Skills(){
     $skillsBox.classList.add("skill-box");
     $titleSkills.classList.add("skills-title-box");
     $ulSkills.classList.add("skills-list");
-   
-
-
     
-
     $titleSkills.innerHTML = `Habilidades`;
     $ulSkills.innerHTML = `
      <li><img class="icon-skills" src="app/assets/icons/react.svg" width="80px" height="80px"></img></li>
@@ -33,17 +29,17 @@ export function Skills(){
      <li><img class="icon-skills" src="app/assets/icons/trello.svg" width="80px" height="80px"></img></<li>
      <li><img class="icon-skills" src="app/assets/icons/terminal.svg" width="80px" height="80px"></img></<li>
      <li><img class="icon-skills" src="app/assets/icons/sass.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/jest.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/jquery.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/webpack.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/figma1.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/photoshop.svg" width="80px" height="80px"></img></<li>
      `;
     
     $skillsConteiner.appendChild($titleSkills);
     $skillsConteiner.appendChild($skillsBoxConteiner);
     $skillsBoxConteiner.appendChild($skillsBox);
     $skillsBox.appendChild($ulSkills);
-    
-    
-    
-   
-
 
     return $skillsConteiner;
 }
