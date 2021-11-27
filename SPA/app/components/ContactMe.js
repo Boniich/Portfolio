bc9d9170cfb4b055e4601e3b77fe958fe5dcecef
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-27 09:46:57 -0300

    a few change about text content and some css class

--- SPA/app/components/ContactMe.js
+++ SPA/app/components/ContactMe.js
@@ -4,16 +4,16 @@ export function ContactMe(){
     $contactMeInfoConteiner = document.createElement("div"),
     $contactMeTextBox = document.createElement("div"),
     $textBox = document.createElement("div"),
-    $tag = document.createElement("p"),
+    $titleContactMe = document.createElement("h1"),
     $email = document.createElement("h1"),
     $text = document.createElement("p");
 
 
     // css
 
-    $tag.classList.add("seccion-tag");
+    $titleContactMe.classList.add("title-sections");
 
-    $contactMe.classList.add("conteiner", "contact");
+    $contactMe.classList.add("conteiner");
     $contactMeInfoConteiner.classList.add("contact-me");
     $contactMeTextBox.classList.add("contact-me-box");
     $text.classList.add("contact-description");
@@ -21,17 +21,17 @@ export function ContactMe(){
 
 
     
-    $tag.innerHTML = "Contactame";
+    $titleContactMe.innerHTML = "Contactame";
     $email.innerHTML = "EzequielDbo25@gmail.com";
-    $text.innerHTML = "Si deseas ponerte en contacto conmigo por trabajo, colaboracion o cualquier otra razon, mandame un mail"
-
-
-    // nodos
-    $textBox.appendChild($tag);
+    $text.innerHTML = `Si deseas ponerte en contacto conmigo por trabajo, colaboracion o cualquier otra razon,
+     mandame un mail o agregame a <a id="link-linkeding" href="https://www.linkedin.com/in/ezequielboninodesarrolladorfrontend/" target="_blank">Linkedin</a>`;
+     
+    
     $textBox.appendChild($email);
     $textBox.appendChild($text);
     $contactMeTextBox.appendChild($textBox);
     $contactMeInfoConteiner.appendChild($contactMeTextBox);
+    $contactMe.appendChild($titleContactMe);
     $contactMe.appendChild($contactMeInfoConteiner);
 
     return $contactMe;
