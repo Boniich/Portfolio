c1db47188e46eda9cd46802d2340381a554500c5
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-28 11:52:29 -0300

    all about skills

---
+++ SPA/app/components/skillSection/SkillSection.js
@@ -0,0 +1,45 @@
+export function SkillSection(){
+
+    const $skillSection = document.createElement("section"),
+    $skillsBoxConteiner = document.createElement("div"),
+    $titleSkills = document.createElement("h6"),
+    $skillsBox = document.createElement("div"),
+    $ulSkills = document.createElement("ul");
+
+    $skillSection.id = "skill-section"
+    $skillSection.classList.add("conteiner");
+    $skillsBoxConteiner.classList.add("skills-box-conteiner");
+    $skillsBox.classList.add("skill-box");
+    $titleSkills.classList.add("title-sections");
+    $ulSkills.classList.add("skills-list");
+    
+    $titleSkills.innerHTML = `Habilidades`;
+    $ulSkills.innerHTML = `
+     <li><img class="icon-skills" src="app/assets/icons/react.svg" width="80px" height="80px"></img></li>
+     <li><img class="icon-skills" src="app/assets/icons/html.svg" width="80px" height="80px"></img></li>
+     <li><img class="icon-skills" src="app/assets/icons/css.svg" width="80px" height="80px"></img></li>
+     <li><img class="icon-skills" src="app/assets/icons/js.svg" width="80px" height="80px"></img></li>
+     <li><img class="icon-skills" src="app/assets/icons/nodejs.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/php.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/mysql.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/c.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/c++.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/java.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/git.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/trello.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/terminal.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/sass.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/jest.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/jquery.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/webpack.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/figma1.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/photoshop.svg" width="80px" height="80px"></img></<li>
+     `;
+    
+    $skillSection.appendChild($titleSkills);
+    $skillSection.appendChild($skillsBoxConteiner);
+    $skillsBoxConteiner.appendChild($skillsBox);
+    $skillsBox.appendChild($ulSkills);
+
+    return $skillSection;
+}
