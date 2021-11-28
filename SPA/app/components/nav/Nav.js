db735f674b35355ced47e8c9cb3a90cda0bfe18a
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-28 11:52:08 -0300

    all about nav

---
+++ SPA/app/components/nav/Nav.js
@@ -0,0 +1,57 @@
+export function Nav(){
+
+    const $navConteiner = document.createElement("div"),
+    $nav = document.createElement("nav"),
+    $label = document.createElement("label"),
+    $ul = document.createElement("ul");
+
+    $navConteiner.id = "nav-id";
+    $navConteiner.classList.add("nav-conteiner"),
+    $nav.classList.add("nav");
+    $label.classList.add("icon");
+    $ul.classList.add("navList");
+
+    
+    // addEventListener("DOMContentLoaded", () =>{
+    //     const button = document.querySelector(".icon");
+
+    //     button.addEventListener("click", ()=>{
+    //         if(button){
+    //             const menu = document.querySelector(".navList");
+    //             menu.classList.toggle("open");
+    //             const n = document.querySelector(".navList.open");
+               
+
+    //             if(n){
+    //                 document.body.style.overflow = "hidden";
+    //             }else{
+    //                 document.body.style.overflow = "scroll";
+    //             }
+                
+    //         }
+    //     })
+    // })
+
+
+    // Creamos <li> y damos textos a los link del menu
+
+    $label.innerHTML = `<i class="fas fa-bars"></i>`;
+
+    $ul.innerHTML = `
+    <li><a href="#/">Inicio</a></li>
+    <li><a href="#aboutMe-section">Sobre Mi</a></li>
+    <li><a href="#skill-section">Habilidades</a><li>
+    <li><a href="#projectIntro-section">Proyectos</a>
+    <ul class="sub-nav">
+    <li><a href="#/proyectos">Mas Proyectos</a></li>
+    </ul></li>
+    <li><a href="#contact">Contactame</a></li>
+    `
+    $nav.appendChild($label);
+    $nav.appendChild($ul);
+
+    $navConteiner.appendChild($nav);
+   
+
+    return $navConteiner;
+}
