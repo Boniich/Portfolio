16c9f2c7edab3c363e80e6545dd65ba32ae7913f
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-11-30 12:18:43 -0300

    added a few icons for give a bit more of style

--- SPA/app/helpers/fakeApi.js
+++ SPA/app/helpers/fakeApi.js
@@ -3,11 +3,13 @@
 export function fakeApi(){
 
     
-    var text = `Mi nombre es Ezequiel, hace casi un año que aprendo de forma autodidacta los distintos lenguages/tecnologias que estan relacionados con la web
-                    he aplicado estos conocimiento realizando distintos proyectos personales.
+    var text = `💻 Mi nombre es Ezequiel, hace casi un año que aprendo de forma autodidacta los distintos lenguages/tecnologias que estan relacionados con la web
+                    y he aplicado estos conocimiento realizando distintos proyectos personales.
                     <br><br>
-                    Tengo varios años metido en la programacion, he programado en varios lenguajes, tales como C o java.<br>
-                    Actualmente estoy en una pausa de la universidad, pero realize varias materias de la Licenciatura de Sistemas en la Universidad de Lanus`;
+                📖 Tengo varios años metido en la programacion, he programado en varios lenguajes, tales como C o java.<br>
+                    Actualmente estoy en una pausa de la universidad, pero realize varias materias de la Licenciatura de Sistemas en la Universidad de Lanus
+                    <br><br>
+                🎯 Mi objetivo personal es trabjar en lugares y con personas que me permitan seguir creciendo no solo como desarrollador, sino tambien como persona`;
 
 
     return text;
