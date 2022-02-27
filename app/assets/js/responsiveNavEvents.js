5d0a45068514d59507d3a7efaaee798a27a47348
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-27 09:29:09 -0300

    style(navEvents-script) change the format of code with prettier

--- app/assets/js/responsiveNavEvents.js
+++ app/assets/js/responsiveNavEvents.js
@@ -1,14 +1,12 @@
+document.addEventListener("click", (e) => {
+  const menu = document.querySelector(".navList");
 
-      document.addEventListener("click", (e) =>{
-        const menu = document.querySelector(".navList");
+  if (e.target.matches(".fas.fa-bars")) {
+    menu.classList.toggle("open");
+    document.body.classList.toggle("hidden");
+  }
+});
 
-        if(e.target.matches(".fas.fa-bars")){
-            menu.classList.toggle("open");
-            document.body.classList.toggle("hidden");
-        }
-
-    })
-
-    window.addEventListener("hashchange", () =>{
-        document.body.classList.remove("hidden");
-    });
+window.addEventListener("hashchange", () => {
+  document.body.classList.remove("hidden");
+});
