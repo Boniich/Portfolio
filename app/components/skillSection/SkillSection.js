6db0133aedf852066cbc4c35a132165617822687
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-05-18 19:32:29 -0300

    add some new skills

--- app/components/skillSection/SkillSection.js
+++ app/components/skillSection/SkillSection.js
@@ -1,20 +1,19 @@
-export function SkillSection(){
-
-    const $skillSection = document.createElement("section"),
+export function SkillSection() {
+  const $skillSection = document.createElement("section"),
     $skillsBoxConteiner = document.createElement("div"),
     $titleSkills = document.createElement("h6"),
     $skillsBox = document.createElement("div"),
     $ulSkills = document.createElement("ul");
 
-    $skillSection.id = "skill-section"
-    $skillSection.classList.add("conteiner");
-    $skillsBoxConteiner.classList.add("skills-box-conteiner");
-    $skillsBox.classList.add("skill-box");
-    $titleSkills.classList.add("title-sections");
-    $ulSkills.classList.add("skills-list");
-    
-    $titleSkills.innerHTML = `Habilidades`;
-    $ulSkills.innerHTML = `
+  $skillSection.id = "skill-section";
+  $skillSection.classList.add("conteiner");
+  $skillsBoxConteiner.classList.add("skills-box-conteiner");
+  $skillsBox.classList.add("skill-box");
+  $titleSkills.classList.add("title-sections");
+  $ulSkills.classList.add("skills-list");
+
+  $titleSkills.innerHTML = `Habilidades`;
+  $ulSkills.innerHTML = `
      <li><img class="icon-skills" src="app/assets/icons/react.svg" width="80px" height="80px"></img></li>
      <li><img class="icon-skills" src="app/assets/icons/html.svg" width="80px" height="80px"></img></li>
      <li><img class="icon-skills" src="app/assets/icons/css.svg" width="80px" height="80px"></img></li>
@@ -34,12 +33,16 @@ export function SkillSection(){
      <li><img class="icon-skills" src="app/assets/icons/webpack.svg" width="80px" height="80px"></img></<li>
      <li><img class="icon-skills" src="app/assets/icons/figma1.svg" width="80px" height="80px"></img></<li>
      <li><img class="icon-skills" src="app/assets/icons/photoshop.svg" width="80px" height="80px"></img></<li>
-     `;
-    
-    $skillSection.appendChild($titleSkills);
-    $skillSection.appendChild($skillsBoxConteiner);
-    $skillsBoxConteiner.appendChild($skillsBox);
-    $skillsBox.appendChild($ulSkills);
+     <li><img class="icon-skills" src="app/assets/icons/redux.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/jira.svg" width="80px" height="80px"></img></<li>
+     <li><img class="icon-skills" src="app/assets/icons/githubProjects.svg" width="80px" height="80px"></img></<li>
 
-    return $skillSection;
+     `;
+
+  $skillSection.appendChild($titleSkills);
+  $skillSection.appendChild($skillsBoxConteiner);
+  $skillsBoxConteiner.appendChild($skillsBox);
+  $skillsBox.appendChild($ulSkills);
+
+  return $skillSection;
 }
