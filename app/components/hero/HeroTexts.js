5f03b422846480423092b8b172c2c5efa104c79c
boniich <ezequieldbo22@hotmail.com>
2022-07-21 17:32:51 -0300

    refactor: make some change in the hero texts

--- app/components/hero/HeroTexts.js
+++ app/components/hero/HeroTexts.js
@@ -1,17 +1,27 @@
 export function HeroTexts() {
   const $HeroTexts = document.createElement("div"),
+    $heroTextContent = document.createElement("div"),
+    $buttonContainer = document.createElement("div"),
+    $downloadCvButton = document.createElement("button"),
     $author = document.createElement("h1"),
     $role = document.createElement("p");
 
   // css
   $HeroTexts.classList.add("hero-texts");
-  $role.classList.add("padding-bottom", "role");
+  $heroTextContent.classList.add("hero-text-content");
+  $buttonContainer.classList.add("hero-button-container");
+  $downloadCvButton.classList.add("downloadCv-button");
+  $role.classList.add("role");
 
   $author.innerHTML = "Ezequiel Bonino";
   $role.innerHTML = "Desarrollador Front end";
+  $downloadCvButton.innerHTML = "Descargar Cv";
 
-  $HeroTexts.appendChild($author);
-  $HeroTexts.appendChild($role);
+  $heroTextContent.appendChild($author);
+  $heroTextContent.appendChild($role);
+  $buttonContainer.appendChild($downloadCvButton);
+  $HeroTexts.appendChild($heroTextContent);
+  $HeroTexts.appendChild($buttonContainer);
 
   return $HeroTexts;
 }
