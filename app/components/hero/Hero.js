02c251a66fe97e95b30fb63ca7b5e622fba2474d
boniich <ezequieldbo22@hotmail.com>
2022-07-20 21:51:05 -0300

    add wave to hero and add some animation for text and img in hero

--- app/components/hero/Hero.js
+++ app/components/hero/Hero.js
@@ -1,32 +1,36 @@
 import { HeroTexts } from "./HeroTexts.js";
 
-export function Hero(){
-
-    const $hero = document.createElement("div"),
+export function Hero() {
+  const $hero = document.createElement("div"),
+    $heroColor = document.createElement("div"),
+    $heroWave = document.createElement("div"),
+    $heroWave2 = document.createElement("div"),
     $heroContent = document.createElement("div"),
     $conteinerPersonalImg = document.createElement("div"),
     $boxPersonalImg = document.createElement("div"),
     $PersonalImg = document.createElement("img");
 
-    $PersonalImg.src = 'app/assets/img/personalImg.jpg';
-    $PersonalImg.id = "personal-img";
+  $PersonalImg.src = "app/assets/img/personalImg.jpg";
+  $PersonalImg.id = "personal-img";
 
-    $hero.classList.add("hero");
-    $heroContent.classList.add("hero-content");
-    $conteinerPersonalImg.classList.add("conteiner-personal-img");
-    $boxPersonalImg.classList.add("box-personal-img");
+  $hero.classList.add("hero");
+  $heroColor.classList.add("hero-color");
+  $heroWave.classList.add("wave", "w1");
+  $heroWave2.classList.add("wave", "w2");
+  $heroContent.classList.add("hero-content");
+  $conteinerPersonalImg.classList.add("conteiner-personal-img");
+  $boxPersonalImg.classList.add("box-personal-img");
 
+  $boxPersonalImg.appendChild($PersonalImg);
 
-    $boxPersonalImg.appendChild($PersonalImg);
+  $heroContent.appendChild(HeroTexts());
 
+  $conteinerPersonalImg.appendChild($boxPersonalImg);
+  $heroContent.appendChild($conteinerPersonalImg);
+  $hero.appendChild($heroColor);
+  $hero.appendChild($heroWave);
+  $hero.appendChild($heroWave2);
+  $hero.appendChild($heroContent);
 
-    $heroContent.appendChild(HeroTexts());
-    $conteinerPersonalImg.appendChild($boxPersonalImg);
-    $heroContent.appendChild($conteinerPersonalImg);
-    $hero.appendChild($heroContent);
-    
-
-
-
-    return $hero; 
+  return $hero;
 }
--- app/components/hero/HeroTexts.js
+++ app/components/hero/HeroTexts.js
@@ -1,23 +1,17 @@
-export function HeroTexts(){
+export function HeroTexts() {
+  const $HeroTexts = document.createElement("div"),
+    $author = document.createElement("h1"),
+    $role = document.createElement("p");
 
-    const $HeroTexts = document.createElement("div"),
-    $tag = document.createElement("p"),
-    $title = document.createElement("h1"),
-    $author = document.createElement("p");
+  // css
+  $HeroTexts.classList.add("hero-texts");
+  $role.classList.add("padding-bottom", "role");
 
-    // css
-    $HeroTexts.classList.add("hero-texts");
-    $tag.classList.add("padding-bottom", "seccion-tag");
-    $title.classList.add("padding-bottom");
+  $author.innerHTML = "Ezequiel Bonino";
+  $role.innerHTML = "Desarrollador Front end";
 
-    $tag.innerHTML = "Portfolio";
-    $title.innerHTML = "Desarrollador Front End";
-    $author.innerHTML = "Por Ezequiel Bonino";
+  $HeroTexts.appendChild($author);
+  $HeroTexts.appendChild($role);
 
-    $HeroTexts.appendChild($tag)
-    $HeroTexts.appendChild($title)
-    $HeroTexts.appendChild($author);
-
-
-    return $HeroTexts;
+  return $HeroTexts;
 }
