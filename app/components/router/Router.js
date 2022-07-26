bed4ea8e4403a9523be612982769fecc96ba9dc7
boniich <ezequieldbo22@hotmail.com>
2022-07-26 19:54:08 -0300

    refactor: turn off the section of more projects

--- app/components/router/Router.js
+++ app/components/router/Router.js
@@ -1,29 +1,32 @@
 import { Main } from "../main/Main.js";
-import { getProyects } from "../../helpers/getProyects.js";
+// import { getProyects } from "../../helpers/getProyects.js";
 import { Hero } from "../hero/Hero.js";
-import { heroProject } from "../proyects/moreProjectHero/heroProject.js";
+// import { heroProject } from "../proyects/moreProjectHero/heroProject.js";
 
-
-export async function Router(){
-
-    const $doc = document,
+export async function Router() {
+  const $doc = document,
     $main = $doc.getElementById("main-conteiner"),
     $heroConteiner = $doc.getElementById("heroConteiner");
 
-    let {hash} = location;
+  let { hash } = location;
 
+  if (
+    !hash ||
+    hash === "#/" ||
+    hash === "#contact" ||
+    hash === "#aboutMe-section" ||
+    hash === "#projectIntro-section" ||
+    hash === "#skill-section"
+  ) {
+    $heroConteiner.appendChild(Hero());
+    $main.appendChild(Main());
+  }
+  //   it will be enabled if i add more project some day
+  // else if (hash === "#/proyectos"){
+  //     $heroConteiner.appendChild(heroProject());
+  //     $main.classList.add("grid-fluid");
+  //     //call async function that render proyects cards
+  //     getProyects($main);
 
-    if(!hash || hash === "#/" || hash === "#contact" || hash === "#aboutMe-section"
-    || hash === "#projectIntro-section"|| hash === "#skill-section"){
-        $heroConteiner.appendChild(Hero());
-        $main.appendChild(Main());
-    }else if (hash === "#/proyectos"){
-        $heroConteiner.appendChild(heroProject());
-        $main.classList.add("grid-fluid");
-        //call async function that render proyects cards
-        getProyects($main);
-
-    }
-
-
+  // }
 }
