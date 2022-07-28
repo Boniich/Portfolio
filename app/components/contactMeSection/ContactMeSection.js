f1ae63d56216382e9c5e6bba4f6f9cc6d24b9341
boniich <ezequieldbo22@hotmail.com>
2022-07-28 19:04:27 -0300

    refactor: make some changes in footer page to improve how it looks

--- app/components/contactMeSection/ContactMeSection.js
+++ app/components/contactMeSection/ContactMeSection.js
@@ -1,6 +1,5 @@
-export function ContactMeSection(){
-
-    const $contactMeSection = document.createElement("section"),
+export function ContactMeSection() {
+  const $contactMeSection = document.createElement("section"),
     $contactMeInfoConteiner = document.createElement("div"),
     $contactMeTextBox = document.createElement("div"),
     $textBox = document.createElement("div"),
@@ -8,33 +7,28 @@ export function ContactMeSection(){
     $email = document.createElement("h1"),
     $text = document.createElement("p");
 
+  // css
 
-    // css
+  $titleContactMe.classList.add("title-sections");
 
-    $titleContactMe.classList.add("title-sections");
+  $contactMeSection.classList.add("conteiner");
+  $contactMeSection.id = "contact";
+  $contactMeInfoConteiner.classList.add("contact-me");
+  $contactMeTextBox.classList.add("contact-me-box");
+  $text.classList.add("contact-description");
+  $textBox.classList.add("contactme-text");
 
-    $contactMeSection.classList.add("conteiner");
-    $contactMeSection.id = "contact";
-    $contactMeInfoConteiner.classList.add("contact-me");
-    $contactMeTextBox.classList.add("contact-me-box");
-    $text.classList.add("contact-description");
-    $textBox.classList.add("contactme-text");
-
-
-    
-    $titleContactMe.innerHTML = "Contactame";
-    $email.innerHTML = "EzequielDbo25@gmail.com";
-    $text.innerHTML = `Si deseas ponerte en contacto conmigo por trabajo, colaboracion o cualquier otra razon,
+  $titleContactMe.innerHTML = "Contactame";
+  $email.innerHTML = "BonichDev@gmail.com";
+  $text.innerHTML = `Si deseas ponerte en contacto conmigo por trabajo, colaboracion o cualquier otra razon,
      mandame un mail o agregame a <a id="link-linkeding" href="https://www.linkedin.com/in/ezequielboninodesarrolladorfrontend/" target="_blank">Linkedin</a>`;
-     
-    
-    $textBox.appendChild($email);
-    $textBox.appendChild($text);
-    $contactMeTextBox.appendChild($textBox);
-    $contactMeInfoConteiner.appendChild($contactMeTextBox);
-    $contactMeSection.appendChild($titleContactMe);
-    $contactMeSection.appendChild($contactMeInfoConteiner);
 
-    return $contactMeSection;
+  $textBox.appendChild($email);
+  $textBox.appendChild($text);
+  $contactMeTextBox.appendChild($textBox);
+  $contactMeInfoConteiner.appendChild($contactMeTextBox);
+  $contactMeSection.appendChild($titleContactMe);
+  $contactMeSection.appendChild($contactMeInfoConteiner);
 
+  return $contactMeSection;
 }
