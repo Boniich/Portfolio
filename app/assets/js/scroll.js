c3431191db78238946910793c8202a6e2d7d7ef2
boniich <ezequieldbo22@hotmail.com>
2022-07-29 21:36:30 -0300

    refactor: make some change in nav to fit it to new portfolio

--- app/assets/js/scroll.js
+++ app/assets/js/scroll.js
@@ -3,15 +3,29 @@ scroll();
 function scroll() {
   addEventListener("scroll", () => {
     const cssClassToChangeNavColor = "change-color-nav";
+    const cssClassChangeColorBarNav = "changeColorIcon";
+    const cssClassChangeColorBrandSign = "changeBrandSign";
     const nav = document.querySelector(".nav");
     const navList = document.querySelector(".navList");
+    const navBar = document.querySelector(".icon");
+    const startBrandSign = document.querySelector(".startBrandSign");
+    const brandName = document.querySelector(".brandName");
+    const endBrandSign = document.querySelector(".endBrandSign");
     let position = window.pageYOffset;
     if (position >= 500) {
       nav.classList.add(cssClassToChangeNavColor);
       navList.classList.add(cssClassToChangeNavColor);
+      navBar.classList.add(cssClassChangeColorBarNav);
+      startBrandSign.classList.add(cssClassChangeColorBrandSign);
+      endBrandSign.classList.add(cssClassChangeColorBrandSign);
+      brandName.classList.add(cssClassChangeColorBrandSign);
     } else {
       nav.classList.remove(cssClassToChangeNavColor);
       navList.classList.remove(cssClassToChangeNavColor);
+      navBar.classList.remove(cssClassChangeColorBarNav);
+      startBrandSign.classList.remove(cssClassChangeColorBrandSign);
+      endBrandSign.classList.remove(cssClassChangeColorBrandSign);
+      brandName.classList.remove(cssClassChangeColorBrandSign);
     }
   });
 }
--- app/components/nav/Nav.js
+++ app/components/nav/Nav.js
@@ -2,19 +2,31 @@ export function Nav() {
   const $navConteiner = document.createElement("div"),
     $nav = document.createElement("nav"),
     $label = document.createElement("label"),
-    $ul = document.createElement("ul");
+    $ul = document.createElement("ul"),
+    $brandContainer = document.createElement("div"),
+    $brand = document.createElement("p"),
+    $brandName = document.createElement("p"),
+    $startBrand = document.createElement("span"),
+    $endBrand = document.createElement("span");
 
   $navConteiner.id = "nav-id";
-  $navConteiner.classList.add("nav-conteiner"), $nav.classList.add("nav");
+  $brandContainer.classList.add("brandContainer");
+  $brand.classList.add("brand");
+  $navConteiner.classList.add("nav-conteiner");
+  $nav.classList.add("nav");
   $label.classList.add("icon");
   $ul.classList.add("navList");
 
   $label.innerHTML = `<i class="fas fa-bars"></i>`;
+  $brandName.innerHTML = `Boniich`;
+  $startBrand.innerHTML = "${ ";
+  $endBrand.innerHTML = " }";
+  $brand.innerHTML = `<span class="startBrandSign">$ { </span><span  class="brandName">Boniich</span><span class="endBrandSign"> }</span>`;
 
   $ul.innerHTML = `
     <li><a href="#/">Inicio</a></li>
     <li><a href="#aboutMe-section">Sobre Mi</a></li>
-    <li><a href="#skill-section">Habilidades</a></li>
+    <li><a href="#skill-section">Stacks</a></li>
     <li><a>Proyectos</a> 
     <ul class="sub-nav">
     <li><a href="#grupalProject-section">Proyectos Grupales</a></li>
@@ -22,7 +34,9 @@ export function Nav() {
     </ul></li>
     <li><a href="#contact">Contactame</a></li>
     `;
-  $nav.appendChild($label);
+  $brandContainer.appendChild($brand);
+  $brandContainer.appendChild($label);
+  $nav.appendChild($brandContainer);
   $nav.appendChild($ul);
 
   $navConteiner.appendChild($nav);
