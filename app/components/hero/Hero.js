f5d0ffb76b19af1e5e3e04408d3d81052b8fc63c
boniich <ezequieldbo22@hotmail.com>
2022-12-23 12:12:15 -0300

    feat: add herotext component inside of hero file to make it private

--- app/components/hero/Hero.js
+++ app/components/hero/Hero.js
@@ -1,32 +1,41 @@
-import { HeroTexts } from "./HeroTexts.js";
-
-export function Hero(){
-
-    const $hero = document.createElement("div"),
+export function Hero() {
+  const $hero = document.createElement("div"),
     $heroContent = document.createElement("div"),
     $conteinerPersonalImg = document.createElement("div"),
     $boxPersonalImg = document.createElement("div"),
     $PersonalImg = document.createElement("img");
 
-    $PersonalImg.src = 'app/assets/img/personalImg.jpg';
-    $PersonalImg.id = "personal-img";
+  $PersonalImg.src = "app/assets/img/personalImg.jpg";
+  $PersonalImg.id = "personal-img";
 
-    $hero.classList.add("hero");
-    $heroContent.classList.add("hero-content");
-    $conteinerPersonalImg.classList.add("conteiner-personal-img");
-    $boxPersonalImg.classList.add("box-personal-img");
+  $hero.classList.add("hero");
+  $heroContent.classList.add("hero-content");
+  $conteinerPersonalImg.classList.add("conteiner-personal-img");
+  $boxPersonalImg.classList.add("box-personal-img");
 
+  $boxPersonalImg.appendChild($PersonalImg);
 
-    $boxPersonalImg.appendChild($PersonalImg);
+  $heroContent.appendChild(HeroTexts());
+  $conteinerPersonalImg.appendChild($boxPersonalImg);
+  $heroContent.appendChild($conteinerPersonalImg);
+  $hero.appendChild($heroContent);
 
+  return $hero;
+}
 
-    $heroContent.appendChild(HeroTexts());
-    $conteinerPersonalImg.appendChild($boxPersonalImg);
-    $heroContent.appendChild($conteinerPersonalImg);
-    $hero.appendChild($heroContent);
-    
+function HeroTexts() {
+  const $HeroTexts = document.createElement("div"),
+    $author = document.createElement("h1"),
+    $role = document.createElement("h2");
 
+  $HeroTexts.classList.add("hero-texts");
+  $role.classList.add("padding-bottom");
 
+  $author.innerHTML = "Ezequiel Bonino (Boniich)";
+  $role.innerHTML = "Game Developer";
 
-    return $hero; 
+  $HeroTexts.appendChild($author);
+  $HeroTexts.appendChild($role);
+
+  return $HeroTexts;
 }
