7ac176a49f63d10c0cbaa044cbb87140a28955d9
boniich <ezequieldbo22@hotmail.com>
2022-12-24 19:56:02 -0300

    feat: re-work card projects

--- app/components/proyects/proyectCards/ProyectsCards.js
+++ app/components/proyects/proyectCards/ProyectsCards.js
@@ -1,8 +1,5 @@
 export function ProyectsCards(props) {
-  let { name, img, description, tag, urlDemo, urlRepo, version, urlVersion } =
-    props;
-  let length = tag.length;
-  let html = "";
+  let { name, img, description, urlDemo, urlRepo } = props;
 
   const $doc = document;
 
@@ -12,9 +9,7 @@ export function ProyectsCards(props) {
     $infoBox = $doc.createElement("div"),
     $titleBox = $doc.createElement("div"),
     $title = $doc.createElement("h1"),
-    $description = $doc.createElement("p"),
-    $titleVersionBox = $doc.createElement("div"),
-    $versionBox = $doc.createElement("div");
+    $description = $doc.createElement("p");
 
   // footer of cards
 
@@ -24,55 +19,54 @@ export function ProyectsCards(props) {
   $footerProyectCards.classList.add("footer-card-conteiner");
   $footerBox.classList.add("footer-box");
 
-  // skill tags
-  const $skillsBox = $doc.createElement("div"),
-    $skillsList = $doc.createElement("ul");
-
   // will be contein title, description, and tags
   const $conteinerBoxCard = $doc.createElement("div");
   $conteinerBoxCard.classList.add("conteinerBoxCard");
 
-  $skillsBox.classList.add("skill-tag-box");
-  $skillsList.classList.add("skill-tag-list");
-
   $card.classList.add("proyectCard");
 
   $img.classList.add("imgCard");
-  $imgBox.classList.add("padding-bottom");
-  $infoBox.classList.add("description-proyects");
-  $titleVersionBox.classList.add("titleVersionBox", "padding-bottom");
+  $imgBox.classList.add("image-box");
+  $infoBox.classList.add("project-description");
   $titleBox.classList.add("title-box");
-  $versionBox.classList.add("versionBox");
 
   // content
   $img.src = `${img}`;
   $title.innerHTML = `${name}`;
   $description.innerHTML = `${description}`;
-  $versionBox.innerHTML = `<a class="version" href="${urlVersion}" target="_blank">v${version}</a>`;
-
-  for (let e = 0; e < length; e++) {
-    html += `<li><p id="${tag[e].idtag}" class="${tag[e].classtag}">${tag[e].tagname}</p></li>`;
-  }
-
-  $skillsList.innerHTML = html;
 
   $footerBox.innerHTML = `
-      <a class="button-proyect" href="${urlDemo}" target="_blank"><span class="proyects-icons"><i class="fas fa-desktop"></i></span>Ver Demo</a>
-      <a class="button-proyect" href="${urlRepo}" target="_blank"><span class="proyects-icons"><i class="fab fa-github"></i></span> Repositorio</a>`;
+       <a class="button-proyect" href="${urlRepo}" target="_blank"><span class="proyects-icons">
+      <i class="fa-solid fa-circle-info"></i></span>Information</a>
+      <a class="button-proyect" href="${urlDemo}" target="_blank"><span class="proyects-icons">
+      <i class="fas fa-desktop"></i></span>Ver Demo</a>
+`;
 
   $imgBox.appendChild($img);
   $conteinerBoxCard.appendChild($imgBox);
   $titleBox.appendChild($title);
-  $titleVersionBox.appendChild($titleBox);
-  $titleVersionBox.appendChild($versionBox);
-  $infoBox.appendChild($titleVersionBox);
+  $infoBox.appendChild(ProjectTagType());
+  $infoBox.appendChild($titleBox);
   $infoBox.appendChild($description);
-  $skillsBox.appendChild($skillsList);
+  $infoBox.appendChild($footerBox);
   $conteinerBoxCard.appendChild($infoBox);
-  $conteinerBoxCard.appendChild($skillsBox);
-  $footerProyectCards.appendChild($footerBox);
+  //$footerProyectCards.appendChild($footerBox);
   $card.appendChild($conteinerBoxCard);
   $card.appendChild($footerProyectCards);
 
   return $card;
+}
+
+function ProjectTagType() {
+  const $tagTypeContainer = document.createElement("div"),
+    $groupTag = document.createElement("div");
+
+  $groupTag.classList.add("tag-type-container");
+  $groupTag.classList.add("group-tag");
+
+  $groupTag.innerHTML = `<span class="proyects-icons"><i class="fa-solid fa-user"></i></span><p>Individual Project</p>`;
+
+  $tagTypeContainer.appendChild($groupTag);
+
+  return $tagTypeContainer;
 }
--- app/components/proyects/proyectIntro/ProjectsIntroSection.js
+++ app/components/proyects/proyectIntro/ProjectsIntroSection.js
@@ -1,42 +1,40 @@
 import { buttons } from "../../../helpers/buttons.js";
 import { getProyects } from "../../../helpers/getProyects.js";
 
-export function ProjectsIntroSection(){
-
-    const $projectsIntroSection = document.createElement("section"),
+export function ProjectsIntroSection() {
+  const $projectsIntroSection = document.createElement("section"),
     $projectTitle = document.createElement("h6"),
     $cardProjectBox = document.createElement("div"),
     $buttonsBox = document.createElement("div");
 
-    // const of helper component buttons
-    const textButton = "Mas Proyectos",
+  // const of helper component buttons
+  const textButton = "Mas Proyectos",
     urlButton = "#/proyectos";
 
-    // css class
-    $projectsIntroSection.id = "projectIntro-section";
-    $projectsIntroSection.classList.add("conteiner");
-    $projectsIntroSection.classList.add("proyects");
-    $cardProjectBox.classList.add("button-conteiner");
-    $buttonsBox.classList.add("button-conteiner");
+  // css class
+  $projectsIntroSection.id = "projectIntro-section";
+  $projectsIntroSection.classList.add("conteiner");
+  $projectsIntroSection.classList.add("proyects");
+  $cardProjectBox.classList.add("button-conteiner");
+  $buttonsBox.classList.add("button-conteiner");
 
+  // title
+  $projectTitle.classList.add("title-sections");
+  $projectTitle.id = "proyectos-titulo";
+  $projectTitle.innerHTML = "Web Developement";
 
-    // title
-    $projectTitle.classList.add("title-sections");
-    $projectTitle.id ="proyectos-titulo";
-    $projectTitle.innerHTML = "Proyectos";
+  $projectsIntroSection.appendChild($projectTitle);
 
-    $projectsIntroSection.appendChild($projectTitle);
-    
-    // Set a special grid for card projects
-    $cardProjectBox.classList.add("grid-fluid");
+  // Set a special grid for card projects
+  $cardProjectBox.classList.add("grid-fluid");
 
-    // Call at ajax for show project cards
-    getProyects($cardProjectBox);
+  // Call at ajax for show project cards
+  getProyects($cardProjectBox);
 
-    $buttonsBox.appendChild(buttons(urlButton, textButton));
+  $buttonsBox.appendChild(buttons(urlButton, textButton));
 
-    $projectsIntroSection.appendChild($cardProjectBox);
-    $projectsIntroSection.appendChild($buttonsBox);
+  $projectsIntroSection.appendChild($cardProjectBox);
+  $projectsIntroSection.appendChild($buttonsBox);
 
-    return $projectsIntroSection;
+  return $projectsIntroSection;
 }
