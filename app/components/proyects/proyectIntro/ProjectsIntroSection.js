2d2417bbaa33f3c64102e53bbbc77c2aee89e88c
boniich <ezequieldbo22@hotmail.com>
2022-12-24 20:41:18 -0300

    feat: delete the button of more projects

--- app/components/proyects/proyectIntro/ProjectsIntroSection.js
+++ app/components/proyects/proyectIntro/ProjectsIntroSection.js
@@ -1,22 +1,15 @@
-import { buttons } from "../../../helpers/buttons.js";
 import { getProyects } from "../../../helpers/getProyects.js";
 
 export function ProjectsIntroSection() {
   const $projectsIntroSection = document.createElement("section"),
     $projectTitle = document.createElement("h6"),
-    $cardProjectBox = document.createElement("div"),
-    $buttonsBox = document.createElement("div");
-
-  // const of helper component buttons
-  const textButton = "Mas Proyectos",
-    urlButton = "#/proyectos";
+    $cardProjectBox = document.createElement("div");
 
   // css class
   $projectsIntroSection.id = "projectIntro-section";
   $projectsIntroSection.classList.add("conteiner");
   $projectsIntroSection.classList.add("proyects");
   $cardProjectBox.classList.add("button-conteiner");
-  $buttonsBox.classList.add("button-conteiner");
 
   // title
   $projectTitle.classList.add("title-sections");
@@ -31,10 +24,7 @@ export function ProjectsIntroSection() {
   // Call at ajax for show project cards
   getProyects($cardProjectBox);
 
-  $buttonsBox.appendChild(buttons(urlButton, textButton));
-
   $projectsIntroSection.appendChild($cardProjectBox);
-  $projectsIntroSection.appendChild($buttonsBox);
 
   return $projectsIntroSection;
 }
