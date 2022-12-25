e9e305a4e51bdfb7e82f82c9d5adcbed4c63bb38
boniich <ezequieldbo22@hotmail.com>
2022-12-24 23:35:21 -0300

    feat: update lets talk email and linkedin

--- app/components/contactMeSection/ContactMeSection.js
+++ app/components/contactMeSection/ContactMeSection.js
@@ -1,6 +1,7 @@
-export function ContactMeSection(){
+import { LINKEDIN_URL } from "../../helpers/helperObjects.js";
 
-    const $contactMeSection = document.createElement("section"),
+export function ContactMeSection() {
+  const $contactMeSection = document.createElement("section"),
     $contactMeInfoConteiner = document.createElement("div"),
     $contactMeTextBox = document.createElement("div"),
     $textBox = document.createElement("div"),
@@ -8,33 +9,28 @@ export function ContactMeSection(){
     $email = document.createElement("h1"),
     $text = document.createElement("p");
 
+  // css
 
-    // css
+  $titleContactMe.classList.add("title-sections");
 
-    $titleContactMe.classList.add("title-sections");
+  $contactMeSection.classList.add("conteiner");
+  $contactMeSection.id = "contact";
+  $contactMeInfoConteiner.classList.add("contact-me");
+  $contactMeTextBox.classList.add("contact-me-box");
+  $text.classList.add("contact-description");
+  $textBox.classList.add("contactme-text");
 
-    $contactMeSection.classList.add("conteiner");
-    $contactMeSection.id = "contact";
-    $contactMeInfoConteiner.classList.add("contact-me");
-    $contactMeTextBox.classList.add("contact-me-box");
-    $text.classList.add("contact-description");
-    $textBox.classList.add("contactme-text");
+  $titleContactMe.innerHTML = "Lets Talk";
+  $email.innerHTML = "BoniichDev@gmail.com";
+  $text.innerHTML = `If you want talk with me for job, collaboration or another reason, you can send me an email or talk with me in 
+  <a id="link-linkeding" href="${LINKEDIN_URL}" target="_blank">Linkedin</a>`;
 
+  $textBox.appendChild($email);
+  $textBox.appendChild($text);
+  $contactMeTextBox.appendChild($textBox);
+  $contactMeInfoConteiner.appendChild($contactMeTextBox);
+  $contactMeSection.appendChild($titleContactMe);
+  $contactMeSection.appendChild($contactMeInfoConteiner);
 
-    
-    $titleContactMe.innerHTML = "Contactame";
-    $email.innerHTML = "EzequielDbo25@gmail.com";
-    $text.innerHTML = `Si deseas ponerte en contacto conmigo por trabajo, colaboracion o cualquier otra razon,
-     mandame un mail o agregame a <a id="link-linkeding" href="https://www.linkedin.com/in/ezequielboninodesarrolladorfrontend/" target="_blank">Linkedin</a>`;
-     
-    
-    $textBox.appendChild($email);
-    $textBox.appendChild($text);
-    $contactMeTextBox.appendChild($textBox);
-    $contactMeInfoConteiner.appendChild($contactMeTextBox);
-    $contactMeSection.appendChild($titleContactMe);
-    $contactMeSection.appendChild($contactMeInfoConteiner);
-
-    return $contactMeSection;
-
+  return $contactMeSection;
 }
---
+++ app/helpers/helperObjects.js
@@ -0,0 +1,1 @@
+export const LINKEDIN_URL = "https://www.linkedin.com/in/boniich/";
