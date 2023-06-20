6a36c0c7f932d1f5190b915f0ba468d64dad74c3
boniich <ezequieldbo25@gmail.com>
2023-06-19 21:58:31 -0300

    refactor: change about me description

--- app/helpers/fakeApi.js
+++ app/helpers/fakeApi.js
@@ -2,21 +2,19 @@ export function fakeApi() {
   var text = `👦 Hello! Mi name is Ezequiel, but you can tell-me <b class="about-me-word">Boniich</b>. I am from Argentina, Buenos Aires.
 
               <br><br>
-              🎮 In the last 4 months i was studing <b class="about-me-word">Game developement</b> using <b class="about-me-word">Unity</b> and <b class="about-me-word">C#</b> for create games.
-              In this time i learn a lot about engine and lenguage
+
+              💻 In the last 2 years i did <b class="about-me-underline-word">Web development</b> creating differents projects, individual and group project,
+              which allow me learn a lot of programming. .
+
+              <br><br>
+
+              💪 Currenly, i am focus in <b class="about-me-word">Laravel</b> for <b class="about-me-underline-word">Web development</b> and <b class="about-me-word">C++</b> 
+              for <b class="about-me-underline-word">Desktop</b> and <b class="about-me-underline-word">Game development</b>
   
               <br><br>
-            
-              💻 Also, in the last two years i did <b class="about-me-word">Web development</b> creating differents projects, individual and group project,
-              which allow me learn a lot of programming and open the way to create games
 
-
-             <br><br>
               📖 I am a round of programming since 6 years, test a bit of several lenguages, like c, java, c++, etc.
-              Also i was in an University, but right now i am in pause of it.
-
-              <br><br>
-              🎯 My goal for this 2023 is work has <b class="about-me-word">Game Developer</b>, it is something that i always wish but always see it to far to make, until now.`;
+              Also i was in an University, but right now i am in pause of it.`;
 
   return text;
 }
