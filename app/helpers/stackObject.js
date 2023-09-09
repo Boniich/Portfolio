136bfbe193a91a936adc479589225217ba2ef5b8
boniich <ezequieldbo25@gmail.com>
2023-09-09 11:34:55 -0300

    feat: update icons of stacks

--- app/helpers/stackObject.js
+++ app/helpers/stackObject.js
@@ -2,12 +2,14 @@ export const OtherStack = ["git", "trello", "terminal", "figma1", "jira"];
 
 export const LaravelStack = [
   "laravel",
+  "docker",
   "livewire",
   "react",
   "html",
   "css",
   "js",
   "mysql2",
+  "postgresql",
 ];
 
 export const CPlusPlus = ["c++"];
