e9f4a82863b3b7e9fea66720f313e51d7cc1f656
boniich <ezequieldbo25@gmail.com>
2023-09-09 11:30:57 -0300

    feat: update cv

--- app/helpers/socialDataConst.js
+++ app/helpers/socialDataConst.js
@@ -2,5 +2,5 @@ export const LINKEDIN_URL = "https://www.linkedin.com/in/boniich/";
 export const GITHUB_URL = "https://github.com/boniich";
 export const YOUTUBE_UTL =
   "https://www.youtube.com/channel/UC50c6pI2XhGQYzI1hmxMiOg";
-export const cv = "app/assets/pdf/Ezequiel-Bonino-CV-portafolio.pdf";
+export const cv = "app/assets/pdf/Ezequiel-Bonino-CV-LARAVEL+REACT.pdf";
 export const EMAIL = "ezequieldbo25@gmail.com";
